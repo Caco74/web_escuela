793033b344b80e7e276c0387a45264c00195a782
Franco Daniel Demetrio <francodemetrio87@gmail.com>
2019-09-15 04:23:55 -0300

    [web_escuela]_002

--- js/pasados.js
+++ js/pasados.js
@@ -12,14 +12,36 @@ $(document).ready(function () {
     pas.innerHTML = "";
     for (let item of datos) {
       if (item.fecha < fechaRef) {
-        pas.innerHTML += `<ul><li class="lead" style="list-style-type: square;"><a class="p-2" href="#">${item.fecha}</a>|</li></ul>`
+        pas.innerHTML += `<ul><li class="lead" style="list-style-type: square;"><a class="p-2" href="#">${item.nombre}</a>|</li></ul>`
       }
     }
 
     //Se crea el contenedor que mostrará los eventos clickeados
-    $("#pasados").after('<div id="contenido_pasados" class="row mb-3" style="justify-content: center;">Nuevo</div>');
-
+    $("#pasados").after('<div id="contenido_pasados" class="row mb-3" style="justify-content: center; display: block; text-align: center">Nuevo</div>');
     $("#contenido_pasados").hide();
+    function mostrarEventos(){
+      //Buscar el link clikeado con el id que pertenece al evento!
+      let datosParse_lugar = JSON.stringify( datos[1].lugar)
+      let datosParse_nombre = JSON.stringify( datos[1].nombre)
+      let datosParse_descripcion = JSON.stringify( datos[1].descripcion)
+      let datosParse_costo = JSON.stringify( datos[1].precio)
+      let datosParse_numInvitados = JSON.stringify( datos[1].invitados)
+      $("#contenido_pasados").html(
+        `
+        <h4>Lugar: ${datosParse_lugar}</h4>
+        <h4>Nombre: ${datosParse_nombre}</h4>
+        <h4>Descripcion: ${datosParse_descripcion}</h4>
+        <h4>Costo: ${datosParse_costo}</h4>
+        <h4>Invitados: ${datosParse_numInvitados}</h4>
+        `
+        );
+      $("#contenido_pasados").slideDown("slow");
+    }
+
+    $(".p-2").click(function(){
+      mostrarEventos();
+    });
+
   })
 
   //Guarda el resultado en variables
