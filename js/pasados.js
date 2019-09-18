58cf4c9c934101c4e94ec7151321994c3a8a6564
Franco Daniel Demetrio <francodemetrio87@gmail.com>
2019-09-18 01:14:53 -0300

    [web_escuela]_004

--- js/pasados.js
+++ js/pasados.js
@@ -27,21 +27,23 @@ $(document).ready(function () {
         return 1;
       }
       return -1;
-    });
+    });    
 
     //Crea un string que contenga el HTML que describe el detalle del evento
-    let html = "";
+    var html = "";
 
     //Recorrer el arreglo y concatena el HTML para cada evento
+    var num = 0;
     for (let item of pasados) {
       html += `
-              <div id="contenedor_lista" class="row mb-3" style="display: block;">
-              <a class="p-2" href="#" id="${item.id}">${item.nombre}</a><br>
-              <cite>${item.fecha +" - "+ item.lugar}</cite><br>
-              <span>${item.descripcion}</span><br>
-              <p>${"Costo: " + item.precio}</p>
-              </div>
-              `
+      <div id="contenedor_lista" class="row mb-3" style="display: block;">
+      <a class="p-2" href="#" id="${num}">${item.nombre}</a><br>
+      <cite>${item.fecha +" - "+ item.lugar}</cite><br>
+      <span>${item.descripcion}</span><br>
+      <p>${"Costo: " + item.precio}</p>
+      </div>
+      `
+      num++;
     }
 
     //Modifica el DOM agregando el html generado
@@ -50,65 +52,25 @@ $(document).ready(function () {
 
     //Crear el contenedor que mostrará los eventos clikeados
     $(".p-2").click(function(){
-      //var ruta = $(this).attr("id");
-      //cargarDatos();
+      const ruta = $(this).attr("id");
       //Buscar el link clikeado con el id que pertenece al evento!
       let cont_pasado = document.getElementById('contenedor_lista');
       cont_pasado.innerHTML = "";
       for (let item of pasados) {
-        if (item.fecha < hoy) {
-          $("#pasados").html(
+        if (item.fecha < hoy) {            
+          $("#pasados").html( 
             `
               <div id="contenido_pasados" class="row mb-3" style="justify-content: center; display: block; text-align: center">
-              <h4>Lugar: ${pasados[item.id].lugar}</h4>
-              <h4>Nombre: ${pasados[item.id].nombre}</h4>
-              <h4>Descripcion: ${pasados[item.id].descripcion}</h4>
-              <h4>Costo: ${pasados[item.id].precio}</h4>
-              <h4>Invitados: ${pasados[item.id].invitados}</h4>
+              <h4>Lugar: ${pasados[ruta].lugar}</h4>
+              <h4>Nombre: ${pasados[ruta].nombre}</h4>
+              <h4>Descripcion: ${pasados[ruta].descripcion}</h4>
+              <h4>Costo: ${pasados[ruta].precio}</h4>
+              <h4>Invitados: ${pasados[ruta].invitados}</h4>
               </div>
-              `
-          );
+            `
+          );          
         }
-        //console.log(item.id);
-
       }
     });
   });
 });
-    
-    //
-
-    /*let pas = document.getElementById('pasados');
-    pas.innerHTML = "";
-    for (let item of datos) {
-      console.log(item.id);      
-      if (item.fecha < hoy) {
-        pas.innerHTML += `<div id="contenido_pasados" class="row mb-3" style="justify-content: center; display: block; text-align: center"><a class="p-2" href="#" id="${item.id}">${item.nombre}</a></div>`
-      }
-    }
-
-    //Se crea el contenedor que mostrará los eventos clickeados
-    $("#pasados").after('<div id="contenido_pasados" class="row mb-3" style="justify-content: center; display: block; text-align: center"></div>');
-    
-    //$("#contenido_pasados").hide(); 
-    $(".p-2").click(function(){
-      var ruta = $(this).attr("id");
-        //Buscar el link clikeado con el id que pertenece al evento!      
-        let cont_pasado = document.getElementById('contenido_pasados');
-        cont_pasado.innerHTML = "";
-        for (let item of datos) {
-          if (item.fecha < hoy) {
-            $("#contenido_pasados").html(
-              `
-              <h4>Lugar: ${JSON.stringify(datos[ruta - 1].lugar)}</h4>
-              <h4>Nombre: ${JSON.stringify(datos[ruta - 1].nombre)}</h4>
-              <h4>Descripcion: ${JSON.stringify(datos[ruta - 1].descripcion)}</h4>
-              <h4>Costo: ${JSON.stringify(datos[ruta - 1].precio)}</h4>
-              <h4>Invitados: ${JSON.stringify(datos[ruta - 1].invitados)}</h4>
-              `
-            );
-          }
-        }
-        //$("#contenido_pasados").slideDown("slow");
-    });*/
-
